0eaf2a54f89b70ce3aa84df7e6ff0abf2746ab70
Mukul Dev <devmukul44@gmail.com>
2015-12-23 01:53:35 +0530

    php mail tag added!

---
+++ contactform_mail.php
@@ -0,0 +1,16 @@
+﻿<?php
+$headers = "Content-Type: text/html; charset=UTF-8";
+//$to = "info@tecvit.in";
+$to = "devmukul44@gmail.com";
+$subject = "contact form email "."From: ".$_POST["email"];
+
+$message ="First Name: ".$_POST["fname"]."\n";
+$message .="Last Name: ".$_POST["lname"]."\n";
+$message .="Project Name: ".$_POST["pname"]."\n";
+$message .="email: ".$_POST["email"]."\n";
+$message .="MESSAGE: ".$_POST["message"]."\n";
+
+echo $message;
+mail($to,$subject,$message,$headers);
+header("location:index.php?err=sent");
+?>
