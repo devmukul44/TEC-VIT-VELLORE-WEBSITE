335d8f7a9df046970c50c31fa4f041df39ad0232
Mukul Dev <devmukul44@gmail.com>
2015-12-28 00:34:51 +0530

    info@tecvit.in added

--- contactform_mail.php
+++ contactform_mail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 $headers = "Content-Type: text/html; charset=UTF-8";
-//$to = "info@tecvit.in";
-$to = "devmukul44@gmail.com";
+$to = "info@tecvit.in";
+//$to = "mukuldev.vit@gmail.com";
 $subject = "contact form email "."From: ".$_POST["email"];
 
 $message ="First Name: ".$_POST["fname"]."\n";
@@ -10,7 +10,7 @@ $message .="Project Name: ".$_POST["pname"]."\n";
 $message .="email: ".$_POST["email"]."\n";
 $message .="MESSAGE: ".$_POST["message"]."\n";
 
-echo $message;
+//echo $message;
 mail($to,$subject,$message,$headers);
 header("location:index.php?err=sent");
 ?>
